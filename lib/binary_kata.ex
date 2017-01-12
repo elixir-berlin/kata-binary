b454159993acbff652a3996c9e480d80cc956581
optikfluffel <optik@fluffel.io>
2017-01-12 20:42:31 +0100

    implement gif_dimensions!

--- lib/binary_kata.ex
+++ lib/binary_kata.ex
@@ -1,10 +1,10 @@
 defmodule BinaryKata do
 
   @utf8_bom <<0xEF, 0xBB, 0xBF>>
-  @jpeg_magic_numbers <<0xFF, 0xD8, 0xFF>>
-  @png_magic_numbers <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
-  @gif_magic_numbers1 <<0x47, 0x49, 0x46, 0x38, 0x37, 0x61>>
-  @gif_magic_numbers2 <<0x47, 0x49, 0x46, 0x38, 0x39, 0x61>>
+  @jpeg_magic <<0xFF, 0xD8, 0xFF>>
+  @png_magic <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
+  @gif_magic1 <<0x47, 0x49, 0x46, 0x38, 0x37, 0x61>>
+  @gif_magic2 <<0x47, 0x49, 0x46, 0x38, 0x39, 0x61>>
 
   @doc """
   Should return `true` when given parameter start with UTF8 Byte-Order-Mark, otherwise `false`.
@@ -47,10 +47,10 @@ defmodule BinaryKata do
   @see https://en.wikipedia.org/wiki/Portable_Network_Graphics
   @see https://en.wikipedia.org/wiki/GIF
   """
-  def image_type!(<<magic::binary-size(3), _::binary>>) when magic == @jpeg_magic_numbers, do: :jfif
-  def image_type!(<<magic::binary-size(8), _::binary>>) when magic == @png_magic_numbers,  do: :png
-  def image_type!(<<magic::binary-size(6), _::binary>>) when magic == @gif_magic_numbers1, do: :gif
-  def image_type!(<<magic::binary-size(6), _::binary>>) when magic == @gif_magic_numbers2, do: :gif
+  def image_type!(<<magic::binary-size(3), _::binary>>) when magic == @jpeg_magic, do: :jfif
+  def image_type!(<<magic::binary-size(8), _::binary>>) when magic == @png_magic,  do: :png
+  def image_type!(<<magic::binary-size(6), _::binary>>) when magic == @gif_magic1, do: :gif
+  def image_type!(<<magic::binary-size(6), _::binary>>) when magic == @gif_magic2, do: :gif
   def image_type!(_), do: :unknown
 
   @doc """
@@ -60,7 +60,13 @@ defmodule BinaryKata do
   `width` will be little-endian in byte 7 and 8.
   `height` will be little-endian in byte 9 and 10.
   """
-  def gif_dimensions!(_), do: raise "TODO: Implement me!"
+  def gif_dimensions!(<<magic::binary-size(6),
+                        width::little-integer-size(16),
+                       height::little-integer-size(16),
+                            _::binary>>) when magic == @gif_magic1 or magic == @gif_magic2 do
+    {width, height}
+  end
+  def gif_dimensions!(_), do: :error
 
   @doc """
   Parsing Payload of a ARP packet. Padding will be omitted.
